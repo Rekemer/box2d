55acedb3149a13de3477f36cf0c2462db0160fc9
Rekemer <89906591+Rekemer@users.noreply.github.com>
2023-05-16 20:22:27 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,11 +1,9 @@
 project "Box2D"
 	kind "StaticLib"
 	language "C++"
-	cppdialect "C++11"
 	staticruntime "off"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
@@ -25,8 +23,8 @@ project "Box2D"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		symbols "on"
+		symbols "On"
 
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
